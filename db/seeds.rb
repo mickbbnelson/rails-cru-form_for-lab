52e520b03c2fa3ca6e7a244be91fe48df93d19e7
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-07-06 09:22:14 -0600

    commit tree c6abd210558f52eded58618e03643a925fdf4a49

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Genre.create([
+#    {name: "Funk"},
+#    {name: "Death Metal"}
+#])
+
+#Artist.create([
+#    {name: "Dr Funkenstein", bio:"Didn't go to 7 years of funk school to be called Mr."},
+#    {name: "Metal Scrapper", bio:"Plays metal how it should be played"}
+#])
+
+Song.create([
+    {name: "Funk up side ya head", artist_id: 1, genre_id: 1},
+    {name: "Demon stuff", artist_id: 2, genre_id: 2}
+])
